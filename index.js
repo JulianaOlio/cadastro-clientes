a779fa0082e331ab20a291a7ead7220265d4dcc8
Juliana_Olio <jullymary@hotmail.com>
2024-04-28 12:01:52 -0300

    atualização menu cadastro cliente

--- index.js
+++ index.js
@@ -4,6 +4,30 @@ const { stdin: input, stdout: output } = require('node:process');
 // criar interface para leitura de dados -> camada de conexão / adaptador
 const rl = readline.createInterface({ input, output });
 
+//array de clientes
+const customers = [];
+
+async function listCustomers(){
+    console.clear();
+    console.log(customers);
+    await rl.question("Pressione Enter para continuar...");
+    printMenu();
+}
+
+async function startRegistration(){
+    const name = await rl.question('Qual o nome do cliente?\n');
+    const address = await rl.question('Qual o endereço do cliente?\n');
+    const id = customers.length > 0
+    ? customers[customers.length - 1].id + 1
+    : 1; // operador ternário
+
+    // variavel name fica na propriedade name, não preciso colocar name = name, por exemplo 
+    customers.push({name, address, id});
+    console.log("Cliente cadastrado com sucesso!")
+    await rl.question("Pressione Enter para continuar...");
+    printMenu();
+}
+
 async function printMenu(){
     console.clear();
     console.log("Menu");
@@ -12,13 +36,17 @@ async function printMenu(){
     console.log("3 - Encerrar");
 
     // tenho uma função callback que é representado pelo answer
-    const answer = await rl.question('Qual opção você deseja?');
+    const answer = await rl.question('Qual opção você deseja?\n');
     
-    if (answer ==="3")
-        process.exit(0);
-    else
-        console.log("Sua escolhe foi: " + answer);
-
+    switch(answer){
+        case "1": startRegistration(); break;
+        case "2": listCustomers(); break;
+        case "3": {
+            console.clear();
+            process.exit(0);
+        }
+        default: console.log("Opção inválida. Tente novamente.");
+    }
         await rl.question('Pressione enter para continuar....')
         printMenu();
     };
